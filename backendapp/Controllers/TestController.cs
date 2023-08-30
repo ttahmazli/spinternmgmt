e3598171b093fdd61cb6827d3504e7efb845db2c
Togrul Z. Tahmazli <t.z.tahmazli@socarpolymer.local>
2023-08-30 11:25:59 +0400

    THIRD

--- backendapp/Controllers/TestController.cs
+++ backendapp/Controllers/TestController.cs
@@ -4,25 +4,48 @@ using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Http;
-using System.Web.Mvc;
 using System.Data;
 using System.Configuration;
 using backendapp.Models;
 
 namespace backendapp.Controllers
 {
+    [RoutePrefix("api/Test")]
     public class TestController : ApiController
     {
         SqlConnection conn =
             new SqlConnection(ConfigurationManager.ConnectionStrings["conn"].ConnectionString);
         SqlCommand cmd = null;
+        SqlDataAdapter adapter = null;
 
         [HttpPost]
-        [Route("Registration")]
-        public string Registration(User user)
+        [Route("Login")]
+        public string Login(User user)
         {
-            cmd = new SqlCommand("usp_Registration", conn);
-            cmd.Parameters.AddWithValue("@Name", user.name);
+            string msg = string.Empty;
+            try
+            {
+                adapter = new SqlDataAdapter("LOGIN", conn);
+                adapter.SelectCommand.CommandType = CommandType.StoredProcedure;
+                adapter.SelectCommand.Parameters.AddWithValue("@NAME", user.name);
+                adapter.SelectCommand.Parameters.AddWithValue("@PASSWORD", user.password);
+                DataTable dt = new DataTable();
+                adapter.Fill(dt);
+                if(dt.Rows.Count > 0)
+                {
+                    msg = "user is valid";
+                }
+                else
+                {
+                    msg = "user in invalid";
+                }
+            }
+            catch (Exception ex)
+            {
+                msg = ex.Message;
+            }
+
+            return msg;
         }
     }
 }
--- backendapp/Models/User.cs
+++ backendapp/Models/User.cs
@@ -8,7 +8,9 @@ namespace backendapp.Models
     public class User
     {
         public int id { get; set; }
-        public string firstName { get; set; }
-        public string lastName { get; set; }         
+        public string name { get; set; }
+        public string surname { get; set; }         
+        public string email { get; set; }         
+        public string password { get; set; }         
     }
 }
