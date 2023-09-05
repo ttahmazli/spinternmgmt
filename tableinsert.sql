e5f389b26bcf1314c66f4a083c22ea614f3a7bab
Togrul Z. Tahmazli <t.z.tahmazli@socarpolymer.local>
2023-09-05 11:13:26 +0400

    table query

---
+++ tableinsert.sql
@@ -0,0 +1,7 @@
+INSERT INTO USERS(NAME,SURNAME,EMAIL,PASSWORD) VALUES('Toghrul', 'Tahmazli', 't.z.tahmazli@socarpolymer.az', 'toghrul1234'),
+													 ('Steve', 'Jobs', 's.jobs@socarpolymer.az', 'steve1234'),
+													 ('Ali', 'Valiev', 'a.valiev@socarpolymer.az', 'ali1234'),
+													 ('Jack', 'London', 'j.london@socarpolymer.az', 'jack1234'),
+													 ('Robert', 'Plant', 'r.plant@socarpolymer.az', 'robert1234');
+
+SELECT * FROM USERS;
